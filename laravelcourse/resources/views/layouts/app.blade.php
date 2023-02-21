1f978ea2733c8bd93fbe809d252f7a54e19a0e95
Jguerra47 <juan.guerra@mercadolibre.com.co>
2023-02-21 12:43:12 -0500

    Activity 10 finished

--- laravelcourse/resources/views/layouts/app.blade.php
+++ laravelcourse/resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
                     <a class="nav-link active" href="{{ route('home.about') }}">About</a>
                     <a class="nav-link active" href="{{ route('home.contact') }}">Contact</a>
                     <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+                    <a class="nav-link active" href="{{ route('product.create') }}">Create product</a>
                 </div>
             </div>
         </div>
@@ -49,7 +50,7 @@
             <small>
                 Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
                     href="https://twitter.com/danielgarax">
-                    Daniel Correa
+                    Daniel Correa - Sebastian Guerra
                 </a>
             </small>
         </div>
